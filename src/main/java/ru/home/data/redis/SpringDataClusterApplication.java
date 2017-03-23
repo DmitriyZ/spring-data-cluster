7572c8cc14d8385d7f35038432ab79cd105dcee1
Zaets Dmitriy <diemon89@gmail.com>
2017-03-23 23:48:40 +0300

    initial commit

---
+++ src/main/java/ru/home/data/redis/SpringDataClusterApplication.java
@@ -0,0 +1,12 @@
+package ru.home.data.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataClusterApplication.class, args);
+    }
+}
